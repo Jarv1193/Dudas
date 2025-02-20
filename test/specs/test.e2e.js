f7cf96703e2d24272747389a825deac3af44a212
Jose Rosales <jantoro1993@gmail.com>
2025-02-19 22:11:01 -0700

    Arreglando y poniendo cosas

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -5,7 +5,10 @@ const ProductoJordanPage = require('../pageobjects/producto.page')
 describe('Navegando en mercado libre', () => {     
 
     it('Verificar el menu de categorias', async () => {
-        
+        await cositas
+        await cositas 2
+
+        otras cosas que necesitamos
         await expect(await InicioPage.menuDeVehiculosInternoEnCategorias).toHaveText('Vehículos')
         await expect(await InicioPage.menuDeSupermercadoInternoEnCategorias).toHaveText('Supermercado')
     })
