9c212e7da6df0c3fa464037ebeb766f32e980b10
Jose Rosales <jantoro1993@gmail.com>
2025-02-19 22:03:00 -0700

    Cositas que borre

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -5,8 +5,7 @@ const ProductoJordanPage = require('../pageobjects/producto.page')
 describe('Navegando en mercado libre', () => {     
 
     it('Verificar el menu de categorias', async () => {
-        await InicioPage.open()
-        await InicioPage.desplegarMenuDeCategorias()
+        
         await expect(await InicioPage.menuDeVehiculosInternoEnCategorias).toHaveText('Vehículos')
         await expect(await InicioPage.menuDeSupermercadoInternoEnCategorias).toHaveText('Supermercado')
     })
